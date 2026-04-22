b048cc789b3cc3344b83910a5e9ffb148f808557
oPoTBp <895421831@qq.com>
2026-04-22 23:39:43 +0800

    提交缓存代码

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using ConsoleApp;
 using ConsoleApp1;
 using System;
 using System.Diagnostics.Metrics;
@@ -34,110 +35,9 @@ public class Program
     //    Console.WriteLine("{person}, {a}");
     //    Console.WriteLine($"{person.ToString()}, {a}");
     //}
-    static int counter = 0;
-    private static readonly object lockObject = new object();
     public static async Task Main()
     {
-        {
-            counter = 0;
-            DateTime start = DateTime.Now;
-
-            List<Thread> threads = new List<Thread>();
-            for (int i = 0; i < 1000; i++)
-            {
-                threads.Add(new Thread(() =>
-                {
-                    for (int j = 0; j < 100000; j++)
-                    {
-                        //lock(lockObject)
-                        {
-                            //counter++;
-                        }
-                        Interlocked.Increment(ref counter);
-                    }
-                }));
-                //threads[i].Start();
-            }
-
-            foreach (Thread t in threads)
-            {
-                t.Start();
-            }
-            foreach (Thread t in threads)
-            {
-                t.Join();
-            }
-            DateTime end = DateTime.Now;
-            double costTime = (end - start).TotalSeconds;
-            Console.WriteLine($"In Interlocked, Final Counter: {counter}, costTime: {costTime}"); // 输出 5000
-        }
-
-        {
-            counter = 0;
-            DateTime start = DateTime.Now;
-
-            List<Thread> threads = new List<Thread>();
-            for (int i = 0; i < 1000; i++)
-            {
-                threads.Add(new Thread(() =>
-                {
-                    for (int j = 0; j < 100000; j++)
-                    {
-                        //lock(lockObject)
-                        {
-                            counter++;
-                        }
-                        //Interlocked.Increment(ref counter);
-                    }
-                }));
-                //threads[i].Start();
-            }
-
-            foreach (Thread t in threads)
-            {
-                t.Start();
-            }
-            foreach (Thread t in threads)
-            {
-                t.Join();
-            }
-            DateTime end = DateTime.Now;
-            double costTime = (end - start).TotalSeconds;
-            Console.WriteLine($"In counter, Final Counter: {counter}, costTime: {costTime}"); // 输出 5000
-        }
-
-        {
-            counter = 0;
-            DateTime start = DateTime.Now;
-
-            List<Thread> threads = new List<Thread>();
-            for (int i = 0; i < 1000; i++)
-            {
-                threads.Add(new Thread(() =>
-                {
-                    for (int j = 0; j < 100000; j++)
-                    {
-                        lock(lockObject)
-                        {
-                            counter++;
-                        }
-                        //Interlocked.Increment(ref counter);
-                    }
-                }));
-                //threads[i].Start();
-            }
-
-            foreach (Thread t in threads)
-            {
-                t.Start();
-            }
-            foreach (Thread t in threads)
-            {
-                t.Join();
-            }
-            DateTime end = DateTime.Now;
-            double costTime = (end - start).TotalSeconds;
-            Console.WriteLine($"In lock, Final Counter: {counter}, costTime: {costTime}"); // 输出 5000
-        }
+        //InterlockedTest.test();
+        ServerCache<string>.Test();
     }
 }
